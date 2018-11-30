9b8f7699fc25b36968c6f1c4ca67423556161b9a
Cody Scott <cody.j.b.scott@gmail.com>
2018-11-30 07:46:43 -0500

    Update Resume

--- main.js
+++ main.js
@@ -2,21 +2,27 @@
   'use strict';
 
   var button = document.getElementById('copyEmail');
-  var copyEmail = new ZeroClipboard(button);
   var message = document.getElementById('message');
 
-  copyEmail.on('ready', function(e) {
-    console.log('Ready to copy');
-    copyEmail.on('aftercopy', function(e) {
-      message.classList.remove('hide');
-      window.setTimeout(function() {
-        message.classList.add('hide-transition');
-      }, 1000);
-      window.setTimeout(function() {
-        message.classList.add('hide');
-        message.classList.remove('hide-transition');
-      }, 3000);
-      console.log('Copied!');
-    });
+  function copyToClipboard(text) {
+    const el = document.createElement('textarea');
+    el.value = text;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  };
+
+  button.addEventListener('click', function() {
+    let emailText = button.dataset.clipboardText;
+    copyToClipboard(emailText);
+    message.classList.remove('hide');
+    window.setTimeout(function() {
+      message.classList.add('hide-transition');
+    }, 1000);
+    window.setTimeout(function() {
+      message.classList.add('hide');
+      message.classList.remove('hide-transition');
+    }, 3000);
   });
 })();
